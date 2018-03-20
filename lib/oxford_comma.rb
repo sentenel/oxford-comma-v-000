12c41846bd030f7e4dbc0bd9edfa691ec640cf25
Seth Wax <sethlwax@gmail.com>
2018-03-20 09:04:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-  array.size > 2 ? array.slice(0, array.size - 1).join(", ") + ", and " + array[-1].to_s : array.join(" and ")
+  if array.size > 2 
+    array.slice(0, array.size - 1).join(", ") + ", and " + array[-1].to_s
+  else
+    array.join(" and ")
+  end
 end
